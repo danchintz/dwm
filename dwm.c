b3ae0bdc07398b1a3a654756a5c358ab3c38202d
Dan Hintz - Arch <daniel.hintz@icloud.com>
2020-05-11 20:53:03 -0400

    changed pkill

--- dwm.c
+++ dwm.c
@@ -857,11 +857,11 @@ drawbar(Monitor *m)
 	Client *c;
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon || 1) { /* status is only drawn on selected monitor */
+	/*if (m == selmon || 1) { [> status is only drawn on selected monitor <]*/
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		sw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - sw, 0, sw, bh, 0, stext, 0);
-	}
+	/*}*/
 
 	for (c = m->clients; c; c = c->next) {
 		occ |= c->tags == 255 ? 0 : c->tags;
@@ -2316,7 +2316,7 @@ updatestatus(void)
 		strcpy(stext, "dwm-"VERSION);
 	else
 		copyvalidchars(stext, rawstext);
-	drawbar(selmon);
+	drawbars();
 }
 
 void
