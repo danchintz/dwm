f09204b177ccaa66744dcf5c8d4e47ac93da4fb2
Dan Hintz - Arch <daniel.hintz@icloud.com>
2020-05-12 14:19:02 -0400

    Made non selected monitor tag not highlighted

--- dwm.c
+++ dwm.c
@@ -875,7 +875,10 @@ drawbar(Monitor *m)
 		continue;
 
 		w = TEXTW(tags[i]);
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+		if(m == selmon)
+			drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+		else
+			drw_setscheme(drw, scheme[SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		x += w;
 	}
